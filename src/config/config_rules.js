f4b6ba83c68e85c688e5eec9489941de5503c787
yyxx2z <870890898@qq.com>
2019-08-02 19:03:24 +0800

    Updated config

--- src/config/config_rules.js
+++ src/config/config_rules.js
@@ -1,13 +1,12 @@
-/* eslint-disable */
 export const items = {
-  war: {//战争规则
-    atk_range: {//伤害浮动范围
+  war: { // 战争规则
+    atk_range: { // 伤害浮动范围
       min: 0.5,
-      max: 1.25,
+      max: 1.25
     },
-    column_num: {//列方阵数
+    column_num: { // 列方阵数
       min: 0,
-      max: 10,
+      max: 10
     }
   }
-};
+}
