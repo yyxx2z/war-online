db01fd79a8008c742c6a8959cdf788fae6eba5ca
wanLiYou <347840227@qq.com>
2019-08-02 18:16:27 +0800

    config update

--- src/config/config_rules.js
+++ src/config/config_rules.js
@@ -1,8 +1,12 @@
 export const items = {
     war: {//战争规则
         atk_range: {//伤害浮动范围
-            left: 0.5,
-            right: 1.25,
+            min: 0.5,
+            max: 1.25,
+        },
+        column_num: {//列方阵数
+            min: 0,
+            max: 10,
         }
     }
 };
