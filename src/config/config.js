e754929f908ca9cd90c1061ea28a70db94bd0e15
wanLiYou <347840227@qq.com>
2019-08-02 14:34:15 +0800

    config_building update

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+//参数配置文件
+import building from './config_building';
+
+export const config = {
+    'global': {},
+    'building': building,
+    //***工种
+    //
+    //***兵种
+    //民兵
+    //步兵
+    //弓箭手
+    //投石器
+    //***商人
+    //商人资源&兑换比例
+    //***
+}
---
+++ src/config/config_building.js
@@ -0,0 +1,66 @@
+export const building = {
+    'description': '建筑',
+    'main_base': {
+        'name': '主基地',
+        'description': '主基地，提供一些物品容量和人口上限',
+        'upgrade': {
+            '1': {
+                'population': '100',
+                'stock': {
+                    'all': '1000'
+                }
+            },
+            'x': {
+                'population': '{1.2x}',
+                'stock': '{1.2x}'
+            }
+        }
+    },
+    'orchard': {
+        'name': '果园',
+        'description': '果园，提供少量食物',
+        'upgrade': {
+            '1': {
+                'fruit_farmer': '2',
+                'stock': {
+                    'food': "100"
+                }
+            },
+            'x': {
+                'fruit_farmer': '2x',
+                'stock': {
+                    'food': "{1.2}"
+                }
+            }
+        }
+    },
+    'mine': {
+        'name': '矿场',
+        'description': '矿场，提供采矿的场所',
+        'upgrade': {
+            '1': {
+                'miner': '2',
+                'stock': {
+                    'mine': "100"
+                }
+            },
+            'x': {
+                'miner': '2x',
+                'stock': {
+                    'mine': "{1.2}"
+                }
+            }
+        }
+    }
+    //***建筑(工位，容量，效率)
+    //主营地
+    //果园
+    //石场
+    //伐木场
+    //研究处
+    //畜牧场
+    //金属加工厂
+    //仓库
+    //*防御建筑
+    //城墙
+}
