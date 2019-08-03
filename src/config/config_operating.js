d7eccd65babca680cd6b4c2238c6c96abe5e62cc
wanLiYou <347840227@qq.com>
2019-08-03 15:20:20 +0800

    config_operating update

--- src/config/config_operating.js
+++ src/config/config_operating.js
@@ -1,79 +1,88 @@
 /* eslint-disable */
 export const operating = {
-    pick_food: {
-        name: '采集食物',
-        description: '自己动手丰衣足食',
-        consume: {},
-        effect: {
-            waiting_time: 0,
-            waiting_after: {
-                resources: {
-                    food: '+1'
-                }
-            }
+  description: '操作参数',
+  pick_food: {
+    name: '采集食物',
+    description: '自己动手丰衣足食',
+    consume: {},//消耗
+    effect: {
+      waiting_time: 0,//操作等待时间 单位：毫秒
+      waiting_after: {//等待后执行的操作
+        resources: {
+          food: '+1'
         }
+      }
+    }
+  },
+  create_people: {
+    name: '造人',
+    description: '点一下就怀孕',
+    consume: {
+      resources: {
+        food: '-15',
+      }
     },
-    create_people: {
-        name: '造人',
-        description: '点一下就怀孕',
-        consume: {
-            food: '-15',
-        },
-        effect: {
-            waiting_time: 10 * 1000,
-            waiting_after: {
-                creature: {
-                    young_child: '+1',
-                }
-            }
+    effect: {
+      waiting_time: 10 * 1000,
+      waiting_after: {
+        creature: {
+          young_child: '+1',
         }
+      }
+    }
+  },
+  train_warrior: {
+    name: '训练战士',
+    description: '训练战士',
+    consume: {
+      resources: {
+        food: '-30',
+      }
     },
-    train_warrior: {
-        name: '训练战士',
-        description: '训练战士',
-        consume: {
-            food: '-30',
-        },
-        effect: {
-            waiting_time: 10 * 1000,
-            waiting_after: {
-                creature: {
-                    adult: '-1',
-                    warrior: '+1',
-                }
-            }
+    effect: {
+      waiting_time: 10 * 1000,
+      waiting_after: {
+        creature: {
+          adult: '-1',
+          warrior: '+1',
         }
+      }
+    }
+  },
+  train_archer: {
+    name: '训练弓箭手',
+    description: '训练弓箭手',
+    consume: {
+      resources: {
+        food: '-30',
+      }
     },
-    train_archer: {
-        name: '训练弓箭手',
-        description: '训练弓箭手',
-        consume: {
-            food: '-30',
-        },
-        effect: {
-            waiting_time: 10 * 1000,
-            waiting_after: {
-                creature: {
-                    adult: '-1',
-                    archer: '+1',
-                }
-            }
+    effect: {
+      waiting_time: 10 * 1000,
+      waiting_after: {
+        creature: {
+          adult: '-1',
+          archer: '+1',
         }
+      }
+    }
+  },
+  train_catapult: {
+    name: '训练投石车',
+    description: '训练投石车',
+    consume: {
+      resources: {
+        food: '-150',
+      }
     },
-    train_catapult: {
-        name: '训练投石车',
-        description: '训练投石车',
-        consume: {
-            food: '-150',
-        },
-        effect: {
-            waiting_time: 10 * 1000,
-            waiting_after: {
-                creature: {
-                    adult: '-5',
-                    catapult: '+1',
-                }
-            }
+    effect: {
+      waiting_time: 10 * 1000,
+      waiting_after: {
+        creature: {
+          adult: '-5',
+          catapult: '+1',
         }
-    },
+      }
+    }
+  }
 }
