ed31ae4f0ac3e573d05af49882168c2bd6814b45
wanLiYou <347840227@qq.com>
2019-08-06 11:12:51 +0800

    config_creature update

--- src/config/config_creature.js
+++ src/config/config_creature.js
@@ -42,19 +42,30 @@ export const creature = {
         food: '-2/s'
       }
     },
-    attributes: {
-      hp: 20,//血量
-      atk: 5,//伤害
-      atk_distance: 1,//攻击距离
-      atk_range: {//伤害浮动范围
-        min: 0.5,
-        max: 1.25,
+    upgrade: {
+      1: {
+        attributes: {
+          hp: 20,//血量
+          atk: 5,//伤害
+          atk_distance: 1,//攻击距离
+          atk_range: {//伤害浮动范围
+            min: 0.5,
+            max: 1.25,
+          },
+          effect_range: 3,//被攻击限制人数，最多被3个单位攻击
+          cover_people: 1,//攻击覆盖人数，最多攻击1个单位
+          atk_interval: 0,//攻击间隔，间隔0回合
+          speed: 1//每回合最大可移动距离
+        }
       },
-      effect_range: 3,//被攻击限制人数，最多被3个单位攻击
-      cover_people: 1,//攻击覆盖人数，最多攻击1个单位
-      atk_interval: 0,//攻击间隔，间隔0回合
-      speed: 1//每回合最大可移动距离
-    },
+      x: {
+        attributes: {
+          hp: '{20+4*x}',
+          atk: '{5+1*x}',
+        }
+      }
+    }
+
   },
   archer: {
     name: '弓箭手',
@@ -64,19 +75,29 @@ export const creature = {
         food: '-2/s'
       }
     },
-    attributes: {
-      hp: 10,
-      atk: 3,
-      atk_distance: 5,
-      atk_range: {
-        min: 0.5,
-        max: 1.25,
+    upgrade: {
+      1: {
+        attributes: {
+          hp: 10,
+          atk: 3,
+          atk_distance: 5,
+          atk_range: {
+            min: 0.5,
+            max: 1.25,
+          },
+          effect_range: 3,
+          cover_people: 1,
+          atk_interval: 0,
+          speed: 1
+        }
       },
-      effect_range: 3,
-      cover_people: 1,
-      atk_interval: 0,
-      speed: 1
-    },
+      x: {
+        attributes: {
+          hp: '{10+4*x}',
+          atk: '{4+1*x}',
+        }
+      }
+    }
   },
   catapult: {
     name: '投石器小队',
@@ -86,18 +107,28 @@ export const creature = {
         food: '-10/s'
       }
     },
-    attributes: {
-      hp: 10,
-      atk: 200,
-      atk_distance: 10,
-      atk_range: {
-        min: 0.5,
-        max: 1.25,
+    upgrade: {
+      1: {
+        attributes: {
+          hp: 200,
+          atk: 200,
+          atk_distance: 10,
+          atk_range: {
+            min: 0.8,
+            max: 1.2,
+          },
+          effect_range: 20,
+          cover_people: 5,
+          atk_interval: 1,
+          speed: 1
+        }
       },
-      effect_range: 20,
-      cover_people: 5,
-      atk_interval: 1,
-      speed: 1
-    },
+      x: {
+        attributes: {
+          hp: '{200+20*x}',
+          atk: '{200+20*x}',
+        }
+      }
+    }
   }
 };
